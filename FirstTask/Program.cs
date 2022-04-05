d7d869d2999a4f8ca1ee1e6328826ec51612840d
Sergey_Utkin <sergey.uty.uty@gmail.com>
2022-04-05 20:42:22 +0300

    Добавьте файлы проекта.

---
+++ FirstTask/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static int FibonachiNumber(int index) => (index == 0 || index == 1 ? index : (FibonachiNumber(index - 2) + FibonachiNumber(index - 1)));
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите индекс по который нужно вывести числа Фибоначи:");
+            int index = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine(FibonachiNumber(i));
+            }
+        }
+    }
+}
---
+++ SecondTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("../../../data/exampleFile.json");
+            Dictionary<string, JArray> dic = JsonConvert.DeserializeObject<Dictionary<string, JArray>>(json);
+            Console.WriteLine("{0,-20} {1,5}\n", "Name", "Values");
+            foreach (var item in dic)
+            {
+                Console.WriteLine("{0,-20} {1,5}\n", item.Key, string.Join(" ", item.Value.Select(jv => (int)jv).ToArray()));
+            }
+        }
+    }
+}
